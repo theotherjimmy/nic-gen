7d85c6e8b12adb68837ff38c07ed94782e16cf02
Jimmy Brisson <theotherjimmy@gmail.com>
2016-07-19 11:05:17 -0500

    Generating all nics

--- NicGen.hs
+++ NicGen.hs
@@ -1,4 +1,31 @@
+import Data.Monoid ((<>))
+import Control.Applicative ((<$>),(<*>))
+import System.Environment.XDG.BaseDir (getUserDataFile)
+import Text.Read (readMaybe)
+import System.IO.Error (catchIOError)
 
+vowel :: [Char]
+vowel = "aoeui"
+
+consanant :: [Char]
+consanant = "bcdfghjklmnpqrstvwxyz"
+
+listOfTwo :: a -> a -> [a]
+listOfTwo a b = [a, b]
+
+listOfThree :: a -> a -> a -> [a]
+listOfThree a b c = [a, b, c]
+
+allNics :: [String]
+allNics = consanantDigraphs <> consanantTrigraphs
+          <> vowelDigraphs <> vowelTrigraphs
+  where consanantDigraphs = listOfTwo <$> consanant <*> vowel
+        vowelDigraphs = listOfTwo <$> vowel <*> consanant
+        consanantTrigraphs = listOfThree <$> consanant <*> vowel <*> consanant
+        vowelTrigraphs = listOfThree <$> vowel <*> consanant <*> vowel
+
+dataFile :: IO FilePath
+dataFile = getUserDataFile "nic-gen" "current-nics"
 
 main :: IO ()
-main = print "Not Implemented"
+main = catchIOError (dataFile >>= readFile >>= return . read) (const $ return allNics) >>= print
