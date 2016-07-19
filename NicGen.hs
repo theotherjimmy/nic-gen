73c09816c0d613bd0a755d2d33ec1301221ba66f
Jimmy Brisson <theotherjimmy@gmail.com>
2016-07-19 17:12:18 -0500

    Ask for directions

--- NicGen.hs
+++ NicGen.hs
@@ -1,9 +1,11 @@
+import Data.List (filter)
 import Data.Monoid ((<>))
 import Control.Applicative ((<$>),(<*>))
 import System.Directory (createDirectoryIfMissing)
 import System.Environment.XDG.BaseDir (getUserDataFile, getUserDataDir)
 import System.IO (hPutStr, withFile, IOMode(..))
 import System.IO.Error (catchIOError)
+import System.Random (randomRIO)
 import Text.Read (readMaybe)
 
 vowel :: [Char]
@@ -32,8 +34,20 @@ dataFile = getUserDataFile "nic-gen" "current-nics"
 dataDir :: IO FilePath
 dataDir = getUserDataDir "nic-gen"
 
+readData :: IO [String]
+readData = catchIOError (dataFile >>= readFile >>= readIO) $ const $ return allNics
+
+writeData :: [String] -> IO ()
+writeData nics = do dataDir >>= createDirectoryIfMissing True
+                    name <- dataFile
+                    withFile name WriteMode (flip hPutStr (show nics))
+
 main :: IO ()
-main = do name <- dataFile
-          nics <- catchIOError (readFile name >>= readIO) $ const $ return allNics
-          dataDir >>= createDirectoryIfMissing True
-          withFile name WriteMode (flip hPutStr (show nics))
+main = do nics <- readData
+          nicToTry <- (nics !!) <$> randomRIO (0, length nics)
+          putStrLn (nicToTry <> " (y|N)?")
+          input <- (== "y") <$> getLine
+          case input of
+            False -> writeData $ filter (/= nicToTry) nics
+            True -> writeData nics
+          wrapper input
